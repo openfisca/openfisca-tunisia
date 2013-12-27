1d45d6f2251cbea7ca0c1761794945637e5db79a
Emmanuel Raviart <eraviart@easter-eggs.com>
2013-12-28 00:48:34 +0100

    Cleanup scenarios module.

--- openfisca_tunisia/scenarios.py
+++ openfisca_tunisia/scenarios.py
@@ -41,27 +41,26 @@ class Scenario(object):
         self.indiv = {}
         # indiv est un dict de dict. La clé est le noi de l'individu
         # Exemple :
-        # 0: {'quifoy': 'vous', 'noi': 0, 'quifam': 'parent 1', 'noipref': 0, 'noidec': 0, 
+        # 0: {'quifoy': 'vous', 'noi': 0, 'quifam': 'parent 1', 'noipref': 0, 'noidec': 0,
         #     'birth': datetime.date(1980, 1, 1), 'quimen': 'pref', 'noichef': 0}
         self.declar = {}
-        
+
         # menage est un dict de dict la clé est la pref
         self.menage = {0:{'loyer':500,'so':3, 'code_postal':69001}}
 
         # on ajoute un individu, déclarant et chef de famille
         self.addIndiv(0, datetime(1975,1,1).date(), 'vous', 'chef')
-    
+
         self.nmen = None
         self.xaxis = None
         self.maxrev = None
         self.same_rev_couple = None
         self.year = None
-    
+
     def copy(self):
         from copy import deepcopy
         return deepcopy(self)
 
-    
     def check_consistency(self):
         '''
         Vérifie que le ménage entré est valide
@@ -77,7 +76,7 @@ class Scenario(object):
 #            if vals['quifoy'] == 'conj' and not vals['quifam'] == 'part':
 #                return u"Un conjoint sur la déclaration d'impôt doit être le partenaire dans la famille"
         return ''
-    
+
     def modify(self, noi, newQuifoy = None, newFoyer = None):
         oldFoyer, oldQuifoy = self.indiv[noi]['noidec'], self.indiv[noi]['quifoy']
         if newQuifoy == None: newQuifoy = oldQuifoy
@@ -94,7 +93,6 @@ class Scenario(object):
             self._assignPerson(noi, quifoy = newQuifoy, foyer = newFoyer)
         self.genNbEnf()
 
-    
     def hasConj(self, noidec):
         '''
         Renvoie True s'il y a un conjoint dans la déclaration 'noidec', sinon False
@@ -103,9 +101,9 @@ class Scenario(object):
             if (vals['noidec'] == noidec) and (vals['quifoy']=='conj'):
                 return True
         return False
-                
+
     def _assignVous(self, noi):
-        ''' 
+        '''
         Ajoute la personne numéro 'noi' et crée son foyer
         '''
         self.indiv[noi]['quifoy'] = 'vous'
@@ -113,8 +111,8 @@ class Scenario(object):
         self.declar.update({noi:{}})
 
     def _assignConj(self, noi, noidec):
-        ''' 
-        Ajoute la personne numéro 'noi' à la déclaration numéro 'noidec' en tant 
+        '''
+        Ajoute la personne numéro 'noi' à la déclaration numéro 'noidec' en tant
         que 'conj' si declar n'a pas de conj. Sinon, cherche le premier foyer sans
         conjoint. Sinon, crée un nouveau foyer en tant que vous.
         '''
@@ -130,20 +128,19 @@ class Scenario(object):
             self._assignVous(noi)
 
     def _assignPac(self, noi, noidec):
-        ''' 
-        Ajoute la personne numéro 'noi' à la déclaration numéro 'noidec' en tant 
+        '''
+        Ajoute la personne numéro 'noi' à la déclaration numéro 'noidec' en tant
         que 'pac'.
         '''
         self.indiv[noi]['quifoy'] = 'pac0'
         self.indiv[noi]['noidec'] = noidec
 
-    
     def _assignPerson(self, noi, quifoy = None, foyer = None, quifam = None, famille = None):
         if quifoy is not None:
             if   quifoy     == 'vous': self._assignVous(noi)
             elif quifoy     == 'conj': self._assignConj(noi, foyer)
             elif quifoy[:3] == 'pac' : self._assignPac(noi, foyer)
-        
+
         self.genNbEnf()
 
     def rmvIndiv(self, noi):
@@ -170,7 +167,7 @@ class Scenario(object):
                 yield person
 
     def addIndiv(self, noi, birth, quifoy, quifam):
-        self.indiv.update({noi:{'birth':birth, 
+        self.indiv.update({noi:{'birth':birth,
                                 'inv': 0,
                                 'activite':0,
                                 'quifoy': 'none',
@@ -184,7 +181,7 @@ class Scenario(object):
 
     def nbIndiv(self):
         return len(self.indiv)
-            
+
     def genNbEnf(self):
         for noi, vals in self.indiv.iteritems():
             if vals.has_key('statmarit'):
@@ -198,7 +195,7 @@ class Scenario(object):
             if vals['quifoy'] == 'conj':
                 statmarit = self.indiv[vals['noidec']]['statmarit']
             vals.update({'statmarit':statmarit})
-                
+
         for noidec, vals in self.declar.iteritems():
             vals.update(self.NbEnfFoy(noidec))
 
@@ -241,7 +238,7 @@ class Scenario(object):
         outputFile = open(fileName, 'wb')
         pickle.dump({'version': VERSION, 'indiv': self.indiv, 'declar': self.declar, 'menage': self.menage}, outputFile)
         outputFile.close()
-    
+
     def openFile(self, fileName):
         inputFile = open(fileName, 'rb')
         S = pickle.load(inputFile)
@@ -250,7 +247,6 @@ class Scenario(object):
         self.declar = S['declar']
         self.menage = S['menage']
 
-
     def populate_datatable(self, datatable):
         '''
         Popualte a datatable from a given scenario
@@ -258,27 +254,26 @@ class Scenario(object):
         from pandas import DataFrame, concat
         import numpy as np
         scenario = self
-        
+
         if self.nmen is None:
             raise Exception('france.scenario: self.nmen should be not None')
-        
-        nmen = self.nmen 
+
+        nmen = self.nmen
         same_rev_couple = self.same_rev_couple
         datatable.NMEN = nmen
         datatable._nrows = datatable.NMEN*len(scenario.indiv)
         datesim = datatable.datesim
         datatable.table = DataFrame()
-    
+
         idmen = np.arange(60001, 60001 + nmen)
-        
+
         for noi, dct in scenario.indiv.iteritems():
             birth = dct['birth']
             age = datesim.year- birth.year
             agem = 12*(datesim.year- birth.year) + datesim.month - birth.month
             noidec = dct['noidec']
             quifoy = datatable.description.get_col('quifoy').enum[dct['quifoy']]
-            
-            
+
             quimen = datatable.description.get_col('quimen').enum[dct['quimen']]
             dct = {'noi': noi*np.ones(nmen),
                    'age': age*np.ones(nmen),
@@ -287,32 +282,32 @@ class Scenario(object):
                    'quifoy': quifoy*np.ones(nmen),
                    'idmen': idmen,
                    'idfoy': idmen*100 + noidec}
-                
+
             datatable.table = concat([datatable.table, DataFrame(dct)], ignore_index = True)
-    
+
         datatable.gen_index(ENTITIES_INDEX)
-    
+
         for name in datatable.col_names:
             if not name in datatable.table:
                 datatable.table[name] = datatable.description.get_col(name)._default
-            
+
         entity = 'men'
         nb = datatable.index[entity]['nb']
         for noi, dct in scenario.indiv.iteritems():
             for var, val in dct.iteritems():
-                if var in ('birth', 'noipref', 'noidec', 'noichef', 'quifoy', 'quimen', 'quifam'): 
+                if var in ('birth', 'noipref', 'noidec', 'noichef', 'quifoy', 'quimen', 'quifam'):
                     continue
                 if not datatable.index[entity] is None:
                     datatable.set_value(var, np.ones(nb)*val, entity, noi)
             del var, val
-            
+
         entity = 'foy'
         nb = datatable.index[entity]['nb']
         for noi, dct in scenario.declar.iteritems():
             for var, val in dct.iteritems():
                 if not datatable.index[entity][noi] is None:
                     datatable.set_value(var, np.ones(nb)*val, entity, noi)
-    
+
         index = datatable.index['men']
         nb = index['nb']
         entity = "men"
@@ -322,29 +317,29 @@ class Scenario(object):
                     datatable.set_value(var, np.ones(nb)*val, entity, noi)
 
             del var, val
-    
+
         if nmen>1:
             if self.maxrev is None:
                 raise Exception('tunisia.utils.Scenario: self.maxrev should not be None')
-            maxrev = self.maxrev      
+            maxrev = self.maxrev
             datatable.MAXREV = maxrev
-            
+
             if self.xaxis is None:
                 raise Exception('tunisia.utils.Scenario: self.xaxis should not be None')
-            
-            xaxis = self.xaxis    
+
+            xaxis = self.xaxis
             axes = axestools.build_axes()
             var = None
-            
+
             for axe in axes:
                 if axe.name == xaxis:
                     datatable.XAXIS = axe.col_name
                     var = axe.col_name
-                    
+
             if var is None:
-                datatable.XAXIS = xaxis 
+                datatable.XAXIS = xaxis
                 var = xaxis
-                        
+
             vls = np.linspace(0, maxrev, nmen)
             if same_rev_couple is True:
                 entity = 'men'
@@ -352,6 +347,6 @@ class Scenario(object):
                 datatable.set_value(var, 0.5*vls, entity, opt = 1)
             else:
                 datatable.set_value(var, vls, entity, opt = 0)
-                
+
             datatable._isPopulated = True
 
