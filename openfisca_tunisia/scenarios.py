b8afc537822d469031d442c0dc3240fa9935241c
Emmanuel Raviart <eraviart@easter-eggs.com>
2014-01-31 22:56:47 +0100

    Remove unused Scenario._isPopulated.

--- openfisca_tunisia/scenarios.py
+++ openfisca_tunisia/scenarios.py
@@ -342,4 +342,3 @@ class Scenario(object):
                 datatable.set_value(var, 0.5*vls, entity, opt = 1)
             else:
                 datatable.set_value(var, vls, entity, opt = 0)
-            datatable._isPopulated = True
