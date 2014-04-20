ec88769a4be2c87d90abdfe975f835a6cdd0ebf1
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-04-20 19:50:18 +0100

    Fix some dates in param.xml

--- openfisca_tunisia/tests/legislation_tests.py
+++ openfisca_tunisia/tests/legislation_tests.py
@@ -22,7 +22,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+import datetime
 import json
 import xml.etree.ElementTree
 
@@ -34,8 +34,8 @@ TaxBenefitSystem = openfisca_tunisia.init_country()
 tax_benefit_system = TaxBenefitSystem()
 
 
-def test_legislation_xml_file():
-    legislation_tree = xml.etree.ElementTree.parse(model.PARAM_FILE)
+def check_legislation_xml_file(year):
+    legislation_tree = xml.etree.ElementTree.parse(tax_benefit_system.PARAM_FILE)
     legislation_xml_json = conv.check(legislationsxml.xml_legislation_to_json)(legislation_tree.getroot(),
         state = conv.default_state)
 
@@ -51,7 +51,7 @@ def test_legislation_xml_file():
             unicode(json.dumps(legislation_xml_json, ensure_ascii = False, indent = 2)),
             ).encode('utf-8'))
 
-    legislation_json = legislationsxml.transform_node_xml_json_to_json(legislation_xml_json)
+    _, legislation_json = legislationsxml.transform_node_xml_json_to_json(legislation_xml_json)
 
     legislation_json, errors = legislations.validate_legislation_json(legislation_json, state = conv.default_state)
     if errors is not None:
@@ -62,3 +62,31 @@ def test_legislation_xml_file():
             unicode(json.dumps(errors, ensure_ascii = False, indent = 2, sort_keys = True)),
             unicode(json.dumps(legislation_json, ensure_ascii = False, indent = 2)),
             ).encode('utf-8'))
+
+    dated_legislation_json = legislations.generate_dated_legislation_json(legislation_json, datetime.date(year, 1, 1))
+    dated_legislation_json, errors = legislations.validate_dated_legislation_json(dated_legislation_json,
+        state = conv.default_state)
+    if errors is not None:
+        errors = conv.embed_error(dated_legislation_json, 'errors', errors)
+        if errors is None:
+            raise ValueError(unicode(json.dumps(dated_legislation_json, ensure_ascii = False, indent = 2)).encode(
+                'utf-8'))
+        raise ValueError(u'{0} for: {1}'.format(
+            unicode(json.dumps(errors, ensure_ascii = False, indent = 2, sort_keys = True)),
+            unicode(json.dumps(dated_legislation_json, ensure_ascii = False, indent = 2)),
+            ).encode('utf-8'))
+
+    compact_legislation = legislations.compact_dated_node_json(dated_legislation_json)
+    if tax_benefit_system.preprocess_legislation_parameters is not None:
+        tax_benefit_system.preprocess_legislation_parameters(compact_legislation)
+
+
+def test_legislation_xml_file():
+    for year in range(2006, 2011):
+        yield check_legislation_xml_file, year
+
+
+if __name__ == '__main__':
+#    test_legislation_xml_file()
+    import nose
+    nose.core.runmodule(argv = [__file__, '-v', 'legislations_tests:test_legislation_xml_file'])
