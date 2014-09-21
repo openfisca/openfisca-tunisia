de906bc38cae4c2f11cc33ce2764de55fa5d3d12
Emmanuel Raviart <emmanuel@raviart.com>
2014-09-21 18:30:36 +0200

    Split TaxScale into subclasses.

--- openfisca_tunisia/model/irpp.py
+++ openfisca_tunisia/model/irpp.py
@@ -460,7 +460,6 @@ def _ir_brut(rni, _P):
     '''
     bar = _P.ir.bareme
     exemption = _P.ir.reforme.exemption
-    bar.t_x()
     rni_apres_exemption = rni * (exemption.active == 0) + rni * (exemption.active == 1) * (rni > exemption.max)
     ir_brut = -bar.calc(rni_apres_exemption)
     return ir_brut
