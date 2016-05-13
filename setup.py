e6bdf568b7e944ef461fd9e0a434959ff89f68ce
Christophe Benz <christophe.benz@gmail.com>
2016-05-13 16:23:08 +0200

    Use ~= compatible release specifier for semantic versioning
    see https://www.python.org/dev/peps/pep-0440/#compatible-release

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     install_requires = [
         'Babel >= 0.9.4',
         'numpy',
-        'OpenFisca-Core >= 0.5dev',
+        'OpenFisca-Core ~= 0.5dev',
         'scipy >= 0.12',
         ],
     message_extractors = {'openfisca_france': [
