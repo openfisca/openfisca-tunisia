6bb85cd94cd474e45b21bed86886cf31cd55e565
Christophe Benz <christophe.benz@gmail.com>
2016-05-13 16:33:27 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     install_requires = [
         'Babel >= 0.9.4',
         'numpy',
-        'OpenFisca-Core ~= 0.5dev',
+        'OpenFisca-Core ~= 1.0',
         'scipy >= 0.12',
         ],
     message_extractors = {'openfisca_france': [
