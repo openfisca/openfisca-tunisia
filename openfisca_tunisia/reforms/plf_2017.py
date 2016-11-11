ad4c63c86933dec2ade589b20e1e849242f636cb
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-11 17:58:04 +0100

    Stub for plf 2017 Extend impot_revenu.bareme (fuzzy included)

---
+++ openfisca_tunisia/reforms/plf_2017.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+from openfisca_core import periods
+from openfisca_core.reforms import Reform, update_legislation
+
+
+def modify_legislation_json(reference_legislation_json_copy):
+    reform_year = 2016
+    reform_period = periods.period('year', reform_year)
+
+    reference_legislation_json_copy = update_legislation(
+        legislation_json = reference_legislation_json_copy,
+        path = ('children', 'ir', 'children', 'bareme', 'brackets', 3, 'rate'),
+        period = reform_period,
+        value = .27,
+        )
+    return reference_legislation_json_copy
+
+
+class plf_2017(Reform):
+    name = u'Projet de Loi de Finances 2017 appliquée aux revenus 2016'
+
+    def apply(self):
+        self.modify_legislation_json(modifier_function = modify_legislation_json)
