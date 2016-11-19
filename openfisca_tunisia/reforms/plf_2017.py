4ab86b61e4605aa306d6b004c6aefb2e2a26c8be
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-19 19:55:52 +0100

    WIP: bareme plf_2017

--- openfisca_tunisia/reforms/plf_2017.py
+++ openfisca_tunisia/reforms/plf_2017.py
@@ -7,15 +7,31 @@ from openfisca_core.reforms import Reform, update_legislation
 
 
 def modify_legislation_json(reference_legislation_json_copy):
-    reform_year = 2016
-    reform_period = periods.period('year', reform_year)
+    reform_year = 2017
+    reform_legislation_subtree = {
+        '@type': 'Scale',
+        'description': "Tranches de l'IR",
+        'brackets': [
+            {
+                'threshold': [{'start': u'2017-01-01'}, {'value': 0.0}],
+                'rate': [{'start': u'2017-01-01'}, {'value': 0.0}],
+                },
+            {
+                'threshold': [{'start': u'2017-01-01'}, {'value': 5000}],
+                'rate': [{'start': u'2017-01-01'}, {'value': 0.27}],
+                },
+            {
+                'threshold': [{'start': u'2017-01-01'}, {'value': 25000}],
+                'rate': [{'start': u'2017-01-01'}, {'value': .30}],
+                },
+            {
+                'threshold': [{'start': u'2017-01-01'}, {'value': 50000}],
+                'rate': [{'start': u'2017-01-01'}, {'value': 0.35}],
+                },
+            ],
+        }
 
-    reference_legislation_json_copy = update_legislation(
-        legislation_json = reference_legislation_json_copy,
-        path = ('children', 'ir', 'children', 'bareme', 'brackets', 3, 'rate'),
-        period = reform_period,
-        value = .27,
-        )
+    reference_legislation_json_copy['children']['impot_revenu']['children']['bareme'] = reform_legislation_subtree
     return reference_legislation_json_copy
 
 
