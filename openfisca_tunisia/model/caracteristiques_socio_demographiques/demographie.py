60be08ec47a442e46e6e6cb96eef344390bec0a4
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-20 21:26:10 +0100

    Improve a bit chef_de_famille

--- openfisca_tunisia/model/caracteristiques_socio_demographiques/demographie.py
+++ openfisca_tunisia/model/caracteristiques_socio_demographiques/demographie.py
@@ -84,7 +84,7 @@ class marie(Variable):
 
 class celibataire(Variable):
     column = BoolCol
-    entity_class = FoyersFiscaux
+    entity_class = Individus
     label = u"Célibataire"
 
     def function(self, simulation, period):
@@ -96,7 +96,7 @@ class celibataire(Variable):
 
 class divorce(Variable):
     column = BoolCol
-    entity_class = FoyersFiscaux
+    entity_class = Individus
     label = u"Divorcé(e)"
 
     def function(self, simulation, period):
@@ -119,32 +119,3 @@ class veuf(Variable):
 class statut_marital(Variable):
     column = PeriodSizeIndependentIntCol(default = 2)
     entity_class = Individus
-
-
-class chef_de_famille(Variable):
-    column = BoolCol()
-    entity_class = Individus
-    # Du point de vue fiscal, est considéré chef de famille :
-    # - L’époux ; DONE
-    # - Le divorcé ou la divorcée qui a la garde des enfants (divorce & enfnats) TODO;
-    # - Le veuf ou la veuve même sans enfants à charge DONE;
-    # - L’adoptant ou l’adoptante (adoptant). TODO
-    # Cependant, l’épouse a la qualité de chef de famille dans les deux cas suivants :
-    # - Lorsqu’elle justifie que le mari ne dispose d’aucune source de revenu durant l’année de réalisation du
-    #   revenu. Tel est le cas d’une femme qui dispose d’un revenu et dont le mari, poursuivant des études, ne dispose
-    #   d’aucun revenu propre. (marie & sexe & revenu_epoux == 0 & revenu_individu > 0) TODO
-    # - Lorsque remariée, elle a la garde d’enfants issus d’un précédent mariage.
-    # Compte tenu de ce qui précède, n’est pas considéré comme chef de famille et ne bénéficie d’aucune déduction :
-    # - Le célibataire ou la célibataire ;
-    # - Le divorcé ou la divorcée qui n’a pas la garde des enfants ;
-    # - La femme durant le mariage (sauf si elle dispose d’un revenu alors que son mari ne dispose d’aucun revenu) ;
-    # - L’époux qui ne dispose pas d’une source de revenu. Dans ce cas, l’épouse acquiert la qualité de chef de famille
-    #   au cas où elle réalise des revenus.
-
-    def function(self, simulation, period):
-        period = period.start.offset('first-of', 'month').period('year')
-        male = simulation.calculate('male', period = period)
-        marie = simulation.calculate('marie', period = period)
-        veuf = simulation.calculate('veuf', period = period)
-        chef_de_famille = veuf | (marie & male)
-        return period, chef_de_famille
