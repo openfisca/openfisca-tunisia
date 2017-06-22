960d8648e110d3d4b390e51a0391972f8a880c0e
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-06-22 16:12:54 +0200

    Update legislation testing

---
+++ openfisca_tunisia/tests/test_legislations.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
+
+from openfisca_tunisia import TunisiaTaxBenefitSystem
+
+
+# Exceptionally for this test do not import TaxBenefitSystem from tests.base.
+tax_benefit_system = TunisiaTaxBenefitSystem()
+
+
+def check_legislation_xml_file(year):
+    compact_legislation = tax_benefit_system.get_legislation(year)
+
+    assert compact_legislation is not None
+
+
+def test_legislation_xml_file():
+    for year in range(2006, datetime.date.today().year + 1):
+        yield check_legislation_xml_file, year
