cbc9942992ebf55ff839558873f09926f313387a
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-06-27 11:55:09 +0200

    Fix legislation tests

--- openfisca_tunisia/tests/test_legislations.py
+++ openfisca_tunisia/tests/test_legislations.py
@@ -11,8 +11,7 @@ tax_benefit_system = TunisiaTaxBenefitSystem()
 
 
 def check_legislation_xml_file(year):
-    compact_legislation = tax_benefit_system.get_legislation(year)
-
+    compact_legislation = tax_benefit_system.get_compact_legislation(year)
     assert compact_legislation is not None
 
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'OpenFisca-Tunisia',
-    version = '0.10.0',
+    version = '0.10.1',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
