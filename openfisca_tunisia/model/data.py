a2ac549e4513718840b78d4be300a777b7586b2f
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-02-03 18:42:19 +0100

    WIP

--- openfisca_tunisia/model/data.py
+++ openfisca_tunisia/model/data.py
@@ -4,4 +4,27 @@
 from openfisca_tunisia.model.base import *
 
 
+class poids(Variable):
+    default_value = 1
+    is_period_size_independent = True
+    value_type = float
+    entity = Individu
+    label = u"Poids de l'individu"
+    definition_period = YEAR
+
+
+class poids_menage(Variable):
+    is_period_size_independent = True
+    value_type = float
+    entity = Menage
+    label = u"Poids du ménage"
+    definition_period = YEAR
+
+
+class poids_foyer_fiscal(Variable):
+    is_period_size_independent = True
+    value_type = float
+    entity = FoyerFiscal
+    label = u"Poids du foyer fiscal"
+    definition_period = YEAR
 
