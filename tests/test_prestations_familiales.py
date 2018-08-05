76db3785078164abd04a3a301ec28ecb22fb148b
sandcha <sandcha@users.noreply.github.com>
2018-08-05 11:29:36 +0200

    Fix contribution_frais_creche syntax errors and remove age_en_mois_benjamin helper

--- tests/test_prestations_familiales.py
+++ tests/test_prestations_familiales.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
+
 from openfisca_tunisia.model.base import *
-from openfisca_tunisia.model.prestations_familiales import age_en_mois_benjamin
 from tests import base
 
 import datetime
@@ -10,7 +11,7 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-def test_age_en_mois_benjamin():
+def test_contribution_frais_creche():
     year = 2011
     month = '2011-01'
     simulation = base.tax_benefit_system.new_scenario().init_single_entity(
@@ -21,20 +22,29 @@ def test_age_en_mois_benjamin():
             ),
         enfants = [
             dict(
+                id = 'Behija',
                 date_naissance = '1992-01-01',
                 ),
             dict(
+                id = 'Hassen',
                 date_naissance = '2002-01-01',
                 ),
-            ],
+            dict(
+                id = 'Thoura',
+                date_naissance = '2008-12-31',
+                )
+            ]
         ).new_simulation(debug = True)
 
     ages_en_mois = simulation.menage.members('age_en_mois', period = month)
-    assert age_en_mois_benjamin(ages_en_mois) == 107
+    age_benjamin = simulation.menage.min(ages_en_mois)[0]
+    assert age_benjamin == 24
+    assert age_benjamin <= base.tax_benefit_system.parameters(year).prestations_familiales.creche.age_max
+
+    contribution_frais_creche = simulation.calculate('contribution_frais_creche', period = year)
+    log.debug(contribution_frais_creche)
+    assert contribution_frais_creche != 0
 
 
 if __name__ == '__main__':
-    test_age_en_mois_benjamin()
-
-
-
+    test_contribution_frais_creche()
