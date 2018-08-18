c1514d90ddab0f3c93062d8b415ca9825f8cc173
sandcha <sandcha@users.noreply.github.com>
2018-08-18 22:16:52 +0100

    Update to OpenFisca-Core v23

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         ),
     include_package_data = True,  # Will read MANIFEST.in
     install_requires = [
-        'OpenFisca-Core >= 22, < 23.0',
+        'OpenFisca-Core >= 23, < 24',
         'PyYAML >= 3.10',
         'scipy >= 0.12',
         ],
