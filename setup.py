5ae0cd5cd74e23c7d0151bc25227f57132ce6400
sandcha <sandcha@users.noreply.github.com>
2019-02-24 23:15:31 +0100

    Bump Core dependency minor version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         ),
     include_package_data = True,  # Will read MANIFEST.in
     install_requires = [
-        'OpenFisca-Core >=25,<27',
+        'OpenFisca-Core >=26,<27',
         'PyYAML >= 3.10',
         'scipy >= 0.12',
         ],
