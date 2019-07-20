a1ad2255409cd8013d2621be1aab0439e3a97f90
sandcha <sandcha@users.noreply.github.com>
2019-07-20 12:16:23 +0200

    Clean up de_net_a_salaire_de_base reform

--- openfisca_tunisia/reforms/de_net_a_salaire_de_base.py
+++ openfisca_tunisia/reforms/de_net_a_salaire_de_base.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division
-
 from openfisca_tunisia.model.base import Reform, Variable, Individu, MONTH, set_input_divide_by_period 
 
 
@@ -10,33 +9,24 @@ try:
 except ImportError:
     fsolve = None
 
-i=0
 
 def calculate_net_from(salaire_de_base, individu, period, requested_variable_names):
-    global i
-    print(">>> ", i)
-    i += 1
-
-    # We're not wanting to calculate salaire_imposable again, 
-    # but instead manually set it as an input variable
-    print('requested_variable_names >>> ', requested_variable_names)
-    
-    ## print('salaire_de_base >>>', individu.get_holder('salaire_de_base').get_array(period))
-    ## print('salaire_de_base', salaire_de_base)
-    ###  individu.get_holder('salaire_de_base').set_input(period, salaire_de_base)
-
     # Work in isolation
     temp_simulation = individu.simulation.clone()
     temp_individu = temp_simulation.individu
-    temp_individu.get_holder('salaire_de_base').delete_arrays(period)
-    temp_individu.get_holder('salaire_de_base').set_input(period, salaire_de_base)
 
     # Calculated variable holders might contain undesired cache
     # (their entity.simulation points to the original simulation above)
     for name in requested_variable_names:
-        temp_individu.get_holder(name).delete_arrays()
-        temp_simulation.computation_stack = temp_simulation.computation_stack[-1:]
-        print(temp_simulation.computation_stack)
+        temp_individu.get_holder(name).delete_arrays(period)
+
+    # Clean 'computation_stack', that is used by -core to check for computation cycles.
+    # The variable 'salaire_imposable' might have been called from variable X,
+    # that will be calculated again in our iterations to compute the 'salaire_net_a_payer'
+    # requested as an input variable, hence producing a cycle error
+    temp_simulation.computation_stack = []
+
+    temp_individu.get_holder('salaire_de_base').set_input(period, salaire_de_base)
 
     # Force recomputing of salaire_net_a_payer
     temp_individu.get_holder('salaire_net_a_payer').delete_arrays()
@@ -53,12 +43,7 @@ class salaire_de_base(Variable):
     set_input = set_input_divide_by_period
 
     def formula(individu, period):
-        # Calcule le salaire de base à partir du salaire net par inversion numérique.
-
-        ### print("salaire_net_a_payer", individu.get_holder('salaire_net_a_payer').get_array(period))
-        # print("assiette_cotisations_sociales", individu.get_holder('assiette_cotisations_sociales').get_array(period))
-        # print("salaire_imposable", individu.get_holder('salaire_imposable').get_array(period))
-        # print("salaire_de_base", individu.get_holder('salaire_de_base').get_array(period))
+        # Use numerical inversion to calculate 'salaire_de_base' from 'salaire_net_a_payer'
         net = individu.get_holder('salaire_net_a_payer').get_array(period)
 
         if net is None:
@@ -66,28 +51,10 @@ class salaire_de_base(Variable):
 
         simulation = individu.simulation
         simulation.period = period
-        # List of variables already calculated. We will need it to remove their holders,
-        # that might contain undesired cache
-        ## requested_variable_names = list(simulation.requested_periods_by_variable_name.keys())
+
+        # List of variables already calculated. 
+        # We will need it to remove their holders, that might contain undesired cache
         requested_variable_names = [variable_period[0] for variable_period in simulation.computation_stack]
-        print("requested_variable_names", requested_variable_names)
-        print("simulation.computation_stack", simulation.computation_stack)
-        if requested_variable_names:
-            # requested_variable_names.remove('assiette_cotisations_sociales')
-            # requested_variable_names.remove('salaire_imposable')
-            requested_variable_names.remove('salaire_de_base')
-            
-        
-        # Clean 'requested_periods_by_variable_name', that is used by -core to check for computation cycles.
-        # avant : requested_periods_by_variable_name = {variable_name: [period1, period2]}
-        # maintenant : computation_stack = [('variable_name', 'period1'), ('variable_name', 'period2')]
-
-
-
-        # This variable, salaire_imposable, might have been called from variable X,
-        # that will be calculated again in our iterations to compute the salaire_net requested
-        # as an input variable, hence producing a cycle error
-        simulation.requested_periods_by_variable_name = dict()
 
         def solve_func(net):
             def innerfunc(essai_salaire_de_base):
