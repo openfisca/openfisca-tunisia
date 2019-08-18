cd56f9b36cdec77d62a1f81a5a477432747d952c
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-08-18 22:48:45 +0200

    Clean tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -37,3 +37,25 @@ def test_basics(one_scenario_arguments):
     simulation = scenario.new_simulation(debug = False)
     period = one_scenario_arguments['period']
     check_run(simulation, period)
+
+
+@pytest.mark.parametrize('period', range(2007, datetime.date.today().year + 1))
+def test_1_parent_axe(period):
+    scenario = init_single_entity(
+        tax_benefit_system.new_scenario(),
+        axes = [[
+            dict(
+                count = 3,
+                name = 'salaire_imposable',
+                max = 100000,
+                min = 0,
+                )
+            ]],
+        period = period,
+        parent1 = dict(date_naissance = datetime.date(period - 40, 1, 1)),
+        )
+    simulation = scenario.new_simulation()
+    assert simulation.calculate('revenu_disponible', period = period) is not None, \
+        "Can't compute revenu_disponible on period {}".format(period)
+    assert simulation.calculate_add('salaire_super_brut', period = period) is not None, \
+        "Can't compute salaire_super_brut on period {}".format(period)
