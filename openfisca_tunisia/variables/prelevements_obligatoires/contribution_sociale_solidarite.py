0fd37627df8eea10b5e2e9ebf46a5db5cd88f8da
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-02-25 19:49:51 +0100

    Fix css for 2020

--- openfisca_tunisia/variables/prelevements_obligatoires/contribution_sociale_solidarite.py
+++ openfisca_tunisia/variables/prelevements_obligatoires/contribution_sociale_solidarite.py
@@ -21,16 +21,38 @@ class contribution_sociale_solidarite(Variable):
         en majorant de un point les taux d’imposition applicables aux tranches de revenu
         prévues par ledit barème et l’impôt sur le revenu déterminé sur la base
         dudit barème d’impôt sans la majoration d'un point des taux d’imposition,
+
+        Art. 39 de la loi de finance 2020
+        La contribution sociale de solidarité ne s'applique pas aux personnes physiques qui réalisent exclusivement
+        les revenus prévus à l'article 25 du code de l'impôt sur le revenu des personnes physiques
+        et de l'impôt sur les sociétés et dont le revenu annuel net ne dépasse pas 5000 dinars
+        après déduction des abattements au titre de la situation et charges de famille
+        prévus à l'article 40 dudit code uniquement.
+
         """
         salaire_imposable = individu('salaire_imposable', period = period)
         deduction_famille_annuelle = individu.foyer_fiscal('deduction_famille', period = period.this_year)
         irpp_mensuel_salarie = individu('irpp_mensuel_salarie', period = period)
 
-        non_exonere, revenu_assimile_salaire_apres_abattement = calcule_base_imposable(
+        non_exonere_irpp, revenu_assimile_salaire_apres_abattement = calcule_base_imposable(
             salaire_imposable, deduction_famille_annuelle, period, parameters)
         bareme_irpp = parameters(period.start).impot_revenu.bareme.copy()
         bareme_css = parameters(period.start).prelevements_sociaux.contribution_sociale_solidarite.salarie
         bareme_irpp.add_tax_scale(bareme_css)
-        return - irpp_mensuel_salarie - non_exonere * bareme_irpp.calc(
-            (12 * revenu_assimile_salaire_apres_abattement - deduction_famille_annuelle)
-            ) / 12
+
+        if period.year >= 2020:
+            non_exonere_css = (
+                (12 * revenu_assimile_salaire_apres_abattement - deduction_famille_annuelle)
+                > parameters(period.start).impot_revenu.exoneration.seuil
+                )
+            return non_exonere_css * (
+                - irpp_mensuel_salarie - non_exonere_irpp * bareme_irpp.calc(
+                    (12 * revenu_assimile_salaire_apres_abattement - deduction_famille_annuelle)
+                    ) / 12
+                )
+        else:
+            return (
+                - irpp_mensuel_salarie - non_exonere_irpp * bareme_irpp.calc(
+                    (12 * revenu_assimile_salaire_apres_abattement - deduction_famille_annuelle)
+                    ) / 12
+                )
