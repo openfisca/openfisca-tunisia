de5fa93ccf74516e29e9b504ba25fdde000ed242
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2024-09-27 17:47:06 +0100

    Migrate setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,51 @@
 #! /usr/bin/env python
 
-"""Tunisia specific model for OpenFisca -- a versatile microsimulation free software"""
+from setuptools import setup, find_namespace_packages
+from pathlib import Path
 
-
-from setuptools import setup, find_packages
-
-
-classifiers = """\
-Development Status :: 2 - Pre-Alpha
-License :: OSI Approved :: GNU Affero General Public License v3
-Operating System :: POSIX
-Programming Language :: Python
-Topic :: Scientific/Engineering :: Information Analysis
-"""
-
-doc_lines = __doc__.split('\n')
-
+# Read the contents of our README file for PyPi
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'OpenFisca-Tunisia',
     version = '0.33.3',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.org',
-    classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
-    description = doc_lines[0],
-    keywords = 'benefit microsimulation social tax tunisia',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        ],
+    description = 'Tunisia tax and benefit system for OpenFisca',
+    keywords = 'benefit tunisia microsimulation social tax',
     license = 'http://www.fsf.org/licensing/licenses/agpl-3.0.html',
-    long_description = '\n'.join(doc_lines[2:]),
+    license_files = ('LICENSE.AGPL.txt',),
     url = 'https://github.com/openfisca/openfisca-tunisia',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     data_files = [
-        ('share/openfisca/openfisca-tunisia', ['CHANGELOG.md', 'LICENSE.AGPL.txt', 'README.md']),
+        'share/openfisca/openfisca-tunisia', 
+        ['CHANGELOG.md', 'README.md'],
         ],
-    extras_require = dict(
-        tests = [
-            'pytest >= 4.0.0, < 8.0.0',
-            ],
-        notebook = [
+    extras_require = {
+        'dev': [
+            'autopep8 >=2.0.2, <3.0',
+            'flake8 >=6.0.0, <7.0.0',
+            'flake8-print >=5.0.0, <6.0.0',
+            'flake8-quotes >=3.3.2',
+            'pytest >=7.2.2, <8.0',
+            'scipy >=1.10.1, <2.0',  # Only used to test de_net_a_brut reform
+            'requests >=2.28.2, <3.0',
+            'yamllint >=1.30.0, <2.0'
+            ],        
+        'notebook' : [
             'ipykernel >= 4.8',
             'jupyter-client >= 5.2',
             'matplotlib >= 2.2',
@@ -44,18 +53,19 @@ setup(
             'nbformat >= 4.4',
             'pandas >= 0.22.0',
             ],
-        survey = [
+        'survey' : [
             'OpenFisca-Survey-Manager >=0.34,<1.0',
             ]
-        ),
+        },
     include_package_data = True,  # Will read MANIFEST.in
     install_requires = [
         'OpenFisca-Core >=40.0.1, <42',
-        'PyYAML >= 3.10',
         'scipy >= 0.12',
         ],
     message_extractors = {'openfisca_tunisia': [
         ('**.py', 'python', None),
         ]},
-    packages = find_packages(exclude=['tests*']),
+    packages = find_namespace_packages(exclude = [
+        'openfisca_tunisia.tests*',
+        ]),
     )
