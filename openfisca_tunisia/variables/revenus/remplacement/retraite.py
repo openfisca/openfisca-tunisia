14265a4907b7bb63b60a8a1840ae00a05d7a9684
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2024-12-15 20:57:44 +0100

    Add CSS, pert d'emploi

--- openfisca_tunisia/variables/revenus/remplacement/retraite.py
+++ openfisca_tunisia/variables/revenus/remplacement/retraite.py
@@ -1,9 +1,10 @@
 from openfisca_tunisia.variables.base import *
 
 
-# Pensions de retraite
+# Pensions
 
-class pension(Variable):
+
+class pension_de_retraite(Variable):
     value_type = float
     label = 'Pension'
     entity = Individu
@@ -11,11 +12,45 @@ class pension(Variable):
     set_input = set_input_divide_by_period
 
 
+class pension_orphelin(Variable):
+    value_type = float
+    label = 'Pension'
+    entity = Individu
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+
+class pension_d_invalidite(Variable):
+    value_type = float
+    label = 'Pension'
+    entity = Individu
+    definition_period = MONTH
+    set_input = set_input_divide_by_period
+
+
+class pension(Variable):
+    value_type = float
+    label = 'Pension'
+    entity = Individu
+    definition_period = YEAR
+    set_input = set_input_divide_by_period
+
+    def formula(individu, period):
+        return (
+            individu('pension_de_retraite', period, options = [ADD])
+            + individu('pension_d_invalidite', period, options = [ADD])
+            + individu('pension_orphelin', period, options = [ADD])
+            )
+
+
 class revenu_assimile_pension(Variable):
     value_type = float
     label = 'Revenus assimilés à des pensions (pensions et rentes viagères)'
     entity = Individu
     definition_period = YEAR
+
+    def formula_2025_01(individu, period):
+        return individu('pension_de_retraite', period, options = [ADD])
 
     def formula(individu, period):
         return individu('pension', period, options = [ADD])
