e829b6e69f4435961c143e7716ea8491cb995902
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2025-02-10 16:28:22 +0100

    Fix amen_social_presence_handicap_lourd with a formula

--- openfisca_tunisia/variables/prestations/non_contributives/amen_social.py
+++ openfisca_tunisia/variables/prestations/non_contributives/amen_social.py
@@ -8,6 +8,8 @@ class amen_social_presence_handicap_lourd(Variable):
     definition_period = MONTH
     # Critères primaires du décret 2020-317 du 19 mai 2020
     # Handicap de niveau 3
+    def formula_2020(menage, period, parameters):
+        return menage.sum(menage.members('handicap', period) >= 3)
 
 
 class amen_social_pas_d_achat_onereux(Variable):
