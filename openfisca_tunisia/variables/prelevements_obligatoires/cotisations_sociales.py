273eeb45ffccfe167541d4ed441bfd1acd894068
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2025-05-13 09:07:56 +0100

    Add test clean prints

--- openfisca_tunisia/variables/prelevements_obligatoires/cotisations_sociales.py
+++ openfisca_tunisia/variables/prelevements_obligatoires/cotisations_sociales.py
@@ -76,7 +76,7 @@ def compute_cotisation(individu, period, cotisation_type = None, bareme_name = N
         if bareme is not None:
             if regime.name == 'rtfr':
                 smic = parameters(period.start).marche_travail.smig_48h_mensuel
-                bareme = bareme.copy()
+                bareme = bareme.copy()  # To avoid modifying the original object
                 bareme = bareme.multiply_thresholds(smic)
 
             cotisation += bareme.calc(
